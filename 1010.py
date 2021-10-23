dc3bc0f5b157a80b520e58ea13a161eb8f75ad52
Alef Santos <alefdeoliveirasantos@gmail.com>
2021-10-22 23:49:17 -0300

    1000 - 1009 - py

--- 1010.py
+++ 1010.py
@@ -1,6 +1,6 @@
-product_1 = [int(input()) if i < 2 else float(input()) for i in range(3)]
-product_2 = [int(input()) if i < 2 else float(input()) for i in range(3)]
+# product_1 = [int(input()) if i < 2 else float(input()) for i in range(3)]
+# product_2 = [int(input()) if i < 2 else float(input()) for i in range(3)]
 
-value = product_1[1] * product_1[2] + product_2[1] * product_2[2]
+# value = product_1[1] * product_1[2] + product_2[1] * product_2[2]
 
-print("VALOR A PAGAR: R$ {:.2f}".format(value))
+# print("VALOR A PAGAR: R$ {:.2f}".format(value))
